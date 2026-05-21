7f94471badb1f1cb1ddf6de1cffb5904684bf158
Tamim Khan <md.tamim.iq.khan@gmail.com>
2026-05-21 21:42:28 +0600

    Created users Table in DB

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,8 +3,21 @@ import config from "../config";
 
 export const sql = neon(config.connectionString);
 
-export const initDB = () => {
+export const initDB = async () => {
    try {
+    await sql`
+    CREATE TABLE IF NOT EXISTS users(
+     id SERIAL PRIMARY KEY,
+     name VARCHAR(20),
+     email VARCHAR(20) UNIQUE NOT NULL,
+     password TEXT NOT NULL,
+     role VARCHAR(10) DEFAULT 'contributor',
+     created_at TIMESTAMP DEFAULT NOW(),
+     updated_at TIMESTAMP DEFAULT NOW()
+    )
+    `;
+
+    
     console.log('Database Connected Successfully!');
    } catch (error) {
      console.log(error);
