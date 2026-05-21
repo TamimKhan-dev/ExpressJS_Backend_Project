5de5b9608c11c390a919a99ad833b37975e70311
Tamim Khan <md.tamim.iq.khan@gmail.com>
2026-05-21 22:13:43 +0600

    Created issues table into DB

--- src/db/index.ts
+++ src/db/index.ts
@@ -4,22 +4,34 @@ import config from "../config";
 export const sql = neon(config.connectionString);
 
 export const initDB = async () => {
-   try {
+  try {
     await sql`
     CREATE TABLE IF NOT EXISTS users(
      id SERIAL PRIMARY KEY,
-     name VARCHAR(20),
-     email VARCHAR(20) UNIQUE NOT NULL,
+     name VARCHAR(255) NOT NULL,
+     email VARCHAR(255) UNIQUE NOT NULL,
      password TEXT NOT NULL,
-     role VARCHAR(10) DEFAULT 'contributor',
+     role VARCHAR(20) DEFAULT 'contributor' CHECK (role IN ('contributor', 'maintainer')),
      created_at TIMESTAMP DEFAULT NOW(),
      updated_at TIMESTAMP DEFAULT NOW()
     )
     `;
 
-    
-    console.log('Database Connected Successfully!');
-   } catch (error) {
-     console.log(error);
-   }
-}
+    await sql`
+      CREATE TABLE IF NOT EXISTS issues(
+       id SERIAL PRIMARY KEY,
+       reporter_id INT NOT NULL,
+       title VARCHAR(150) NOT NULL,
+       description TEXT NOT NULL,
+       type TEXT CHECK (type IN ('bug', 'feature_request')),
+       status TEXT DEFAULT 'open' CHECK (status IN ('open', 'in_progress', 'resolved')),
+       created_at TIMESTAMP DEFAULT NOW(),
+       updated_at TIMESTAMP DEFAULT NOW()
+      )
+      `;
+
+    console.log("Database Connected Successfully!");
+  } catch (error) {
+    console.log(error);
+  }
+};
