a5ed74d784dbd262e1bde785dd9a3eaa32788a2c
Tamim Khan <md.tamim.iq.khan@gmail.com>
2026-05-23 11:23:30 +0600

    Create global error handler and CORS configuration

--- src/app.ts
+++ src/app.ts
@@ -6,12 +6,17 @@ import express, {
 import config from "./config";
 import { authRoute } from "./modules/auth/auth.route";
 import { issuesRoute } from "./modules/issues/issues.route";
+import cors from "cors";
+import globalErrorHandler from "./middleware/globalErrorHandler";
 
 const app: Application = express();
 
 app.use(express.json());
 app.use(express.text());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+  origin: "http://localhost:5000"
+}))
 
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json({
@@ -21,6 +26,9 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 app.use("/api/auth", authRoute);
-app.use("/api/issues", issuesRoute)
+app.use("/api/issues", issuesRoute);
+
+
+app.use(globalErrorHandler);
 
 export default app;
